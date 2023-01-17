a2d7a76625a30b28233d6bbb47e839915f06cd18
Adarsh <adarsh800251@gmail.com>
2023-01-18 02:53:01 +0530

    backend set

---
+++ Actions.js
@@ -0,0 +1,10 @@
+const ACTIONS = {    // define the actions
+    JOIN:'join',
+    JOINED:'joined',
+    DISCONNECTED:'disconnected',
+    SYNC_CODE:'sync-code',
+    CODE_CHANGE:'code-change',
+    LEAVE:'leave'
+}
+
+module.exports = ACTIONS;
